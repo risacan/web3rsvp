0ff3f71de19ac8a0c9e74389474b94d53ec2b91f
Risa Watanabe Harada <supikateriburu@gmail.com>
2022-08-05 12:49:14 +0900

    :new: Add struct for RSVP

--- contracts/Web3RSVP.sol
+++ contracts/Web3RSVP.sol
@@ -1,2 +1,16 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.9;
+
+contract Web3RSVP {
+    struct CreateEvent {
+        bytes32 eventId;
+        string eventDataCID;
+        address eventOwner;
+        uint256 eventTimestamp;
+        uint256 deposit;
+        uint256 maxCapacity;
+        address[] confirmedRSVPs;
+        address[] claimedRSVPs;
+        bool paidOut;
+    }
+}
