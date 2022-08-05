a9426194b2898aeae7adcfacc1d3895b2eee458b
Risa Watanabe Harada <supikateriburu@gmail.com>
2022-08-05 22:38:27 +0900

    :sparkles: Add createNewRSVP function

--- contracts/Web3RSVP.sol
+++ contracts/Web3RSVP.sol
@@ -47,4 +47,20 @@ contract Web3RSVP {
         claimedRSVPs,
         false
     )
+
+    function createNewRSVP(bytes32 eventId) external payable {
+        CreateEvent storage myEvent = idToEvent[eventId]
+
+        require(msg.value == myEvent.deposit, "NOT ENOUGH");
+
+        require(block.timestamp <= myEvent.eventTimestamp, "Already happened")
+
+        require(myEvent.confirmedRSVPs.length < myEvent.maxCapacity, "This event has reached capacity");
+
+        for (uint8 i = 0, i < myEvent.confirmedRSVPs.lengths; i++) {
+            require(myEvent.confirmedRSVPs[i] != msg.sender, "Already confirmed");
+        }
+
+        myEvent.confirmed.push(payable(msg.sender))
+    }
 }
